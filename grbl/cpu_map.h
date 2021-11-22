f0e674ac838419743a9ce20c9ea432d25888c483
hasz78 <81359379+hasz78@users.noreply.github.com>
2021-11-22 21:46:19 +0100

    set bits

--- grbl/cpu_map.h
+++ grbl/cpu_map.h
@@ -405,17 +405,17 @@
 #ifdef CPU_MAP_STM32F103_IL1
 
   // Define step pulse output pins. NOTE: All step bit pins must be on the same port.
-#define STEP_PORT       GPIOA
-#define RCC_STEP_PORT   RCC_APB2Periph_GPIOA
-#define X_STEP_BIT      0  
-#define Y_STEP_BIT      1  
-#define Z_STEP_BIT      2
+#define STEP_PORT       GPIOB
+#define RCC_STEP_PORT   RCC_APB2Periph_GPIOB
+#define X_STEP_BIT      8  
+#define Y_STEP_BIT      9  
+#define Z_STEP_BIT      4
 #define STEP_MASK       ((1<<X_STEP_BIT)|(1<<Y_STEP_BIT)|(1<<Z_STEP_BIT)) // All step bits
 
   // Define step direction output pins. NOTE: All direction pins must be on the same port.
 #define DIRECTION_PORT      GPIOA
 #define RCC_DIRECTION_PORT   RCC_APB2Periph_GPIOA
-#define X_DIRECTION_BIT   3  
+#define X_DIRECTION_BIT   6  
 #define Y_DIRECTION_BIT   4  
 #define Z_DIRECTION_BIT   5
 #define DIRECTION_MASK    ((1<<X_DIRECTION_BIT)|(1<<Y_DIRECTION_BIT)|(1<<Z_DIRECTION_BIT)) // All direction bits
@@ -423,7 +423,7 @@
   // Define stepper driver enable/disable output pin.
 #define STEPPERS_DISABLE_PORT   GPIOA
 #define RCC_STEPPERS_DISABLE_PORT RCC_APB2Periph_GPIOA
-#define STEPPERS_DISABLE_BIT    6  
+#define STEPPERS_DISABLE_BIT    8  
 #define STEPPERS_DISABLE_MASK   (1<<STEPPERS_DISABLE_BIT)
 #define SetStepperDisableBit() GPIO_SetBits(STEPPERS_DISABLE_PORT,STEPPERS_DISABLE_MASK)
 #define ResetStepperDisableBit() GPIO_ResetBits(STEPPERS_DISABLE_PORT,STEPPERS_DISABLE_MASK)
@@ -431,13 +431,13 @@
 
   // Define homing/hard limit switch input pins and limit interrupt vectors. 
   // NOTE: All limit bit pins must be on the same port
-#define LIMIT_PIN        GPIOB
-#define LIMIT_PORT       GPIOB
-#define RCC_LIMIT_PORT   RCC_APB2Periph_GPIOB
-#define GPIO_LIMIT_PORT  GPIO_PortSourceGPIOB
-#define X_LIMIT_BIT      10  
-#define Y_LIMIT_BIT      11  
-#define Z_LIMIT_BIT      12  
+#define LIMIT_PIN        GPIOA
+#define LIMIT_PORT       GPIOA
+#define RCC_LIMIT_PORT   RCC_APB2Periph_GPIOA
+#define GPIO_LIMIT_PORT  GPIO_PortSourceGPIOA
+#define X_LIMIT_BIT      11  
+#define Y_LIMIT_BIT      12  
+#define Z_LIMIT_BIT      13  
 
 #define LIMIT_MASK       ((1<<X_LIMIT_BIT)|(1<<Y_LIMIT_BIT)|(1<<Z_LIMIT_BIT)) // All limit bits
 
@@ -487,13 +487,13 @@
 #ifdef VARIABLE_SPINDLE
 
   // NOTE: On the 328p, these must be the same as the SPINDLE_ENABLE settings.
-#define SPINDLE_PWM_FREQUENCY       10000                   // KHz
-#define SPINDLE_PWM_DDR	            GPIOA
-#define SPINDLE_PWM_PORT            GPIOA
-#define RCC_SPINDLE_PWM_PORT        RCC_APB2Periph_GPIOA
-#define SPINDLE_PWM_BIT	            8    
+#define SPINDLE_PWM_FREQUENCY       1000                   // KHz
+#define SPINDLE_PWM_DDR	            GPIOB
+#define SPINDLE_PWM_PORT            GPIOB
+#define RCC_SPINDLE_PWM_PORT        RCC_APB2Periph_GPIOB
+#define SPINDLE_PWM_BIT	            1    
 #endif // End of VARIABLE_SPINDLE
-#define SPINDLE_PWM_MAX_VALUE       (1000000 / SPINDLE_PWM_FREQUENCY)
+#define SPINDLE_PWM_MAX_VALUE       (100000 / SPINDLE_PWM_FREQUENCY)
 #ifndef SPINDLE_PWM_MIN_VALUE
 #define SPINDLE_PWM_MIN_VALUE   1   // Must be greater than zero.
 #endif
@@ -501,7 +501,7 @@
 #define SPINDLE_PWM_RANGE         (SPINDLE_PWM_MAX_VALUE-SPINDLE_PWM_MIN_VALUE)
 
   //  Port A                                         Port B
-  //   0      X_STEP_BIT                             
+  //   0      X_STEP_BIT                             SPINDLE_PWM_BIT
   //   1      Y_STEP_BIT                            
   //   2      Z_STEP_BIT                               
   //   3      X_DIRECTION_BIT                       COOLANT_FLOOD_BIT
@@ -509,7 +509,7 @@
   //   5      Z_DIRECTION_BIT                       CONTROL_RESET_BIT
   //   6      STEPPERS_DISABLE_BIT                  CONTROL_FEED_HOLD_BIT    
   //   7                                            CONTROL_CYCLE_START_BIT
-  //   8      SPINDLE_PWM_BIT                       CONTROL_SAFETY_DOOR_BIT    
+  //   8                                             CONTROL_SAFETY_DOOR_BIT    
   //   9                             
   //   10                                            X_LIMIT_BIT
   //   11                                            Y_LIMIT_BIT
